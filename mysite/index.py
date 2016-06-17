4034d1ee0b33523091520f075bf3930d86648c1d
Rohith Asrk <rohith.asrk@gmail.com>
2016-06-18 02:19:17 +0530

    Added some views and a default homepage for the project

---
+++ mysite/index.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def tem(request):
+	return HttpResponse("Hey")
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 
+from . import index
+
 urlpatterns = [
+    url(r'^$', index.tem ,name='tem'),
     url(r'^admin/', admin.site.urls),
     url(r'^polls/',include('polls.urls')),
 ]
