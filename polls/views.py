005c4d757111c521e68a4d0dc5a25f76021a800e
Rohith Asrk <rohith.asrk@gmail.com>
2016-06-18 02:33:34 +0530

    404error raiser

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from .models import Question
-from django.template import loader
-# Create your views here.
+#from django.template import loader
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -13,11 +12,27 @@ def index(request):
 	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
+	try:
+		question=Question.object.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+
 	return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
+	try:
+                question=Question.object.get(pk=question_id)
+        except Question.DoesNotExist:
+                raise Http404("Question does not exist")
+
 	reponse = "You're looking at the results of question %s."
 	return HttpResponse(response % question_id)
 
 def vote(request, question_id):
+	try:
+                question=Question.object.get(pk=question_id)
+        except Question.DoesNotExist:
+                raise Http404("Question does not exist")
+
 	return HttpResponse("You're voting on question %s." % question_id)
+
