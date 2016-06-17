e5f00afd172a1381fd4fe6f11dc004072195041b
Rohith Asrk <rohith.asrk@gmail.com>
2016-06-18 02:59:36 +0530

    Namespaced URL names

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns=[
+app_name = 'polls'
+urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
