b545409ad9415f7b70e37c69f59a56d57fcca030
Rohith Asrk <rohith.asrk@gmail.com>
2016-06-19 20:18:21 +0530

    Fixed a datetime error

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.utils import timezone
+import datetime as dt
 
 #@python_2_unicode_compatible
 class Question(models.Model):
@@ -9,7 +11,7 @@ class Question(models.Model):
 	def __str__(self):
 		return self.question_text
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		return self.pub_date >= timezone.now() - dt.timedelta(days=1)
 	
 #@python_2_unicode_compatible
 class Choice(models.Model):
