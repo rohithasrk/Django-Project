ed6d550867069cb5a92986033285df86c6193638
Rohith Asrk <rohith.asrk@gmail.com>
2016-06-22 01:39:14 +0530

    Admin form customization

--- polls/admin.py
+++ polls/admin.py
@@ -5,7 +5,7 @@ from .models import Question, Choice
 #admin.site.register(Question)
 #admin.site.register(Choice)
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
 	model=Choice
 	extra=3
 
@@ -15,5 +15,7 @@ class QuestionAdmin(admin.ModelAdmin):
 	('Data information',{'fields':['pub_date'],'classes':['collapse']}),
 	]
 	inlines = [ChoiceInline]
+	list_display=('question_text','pub_date','was_published_recently')
+	list_filter=['pub_date']
 
 admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,10 @@ class Question(models.Model):
 		return self.question_text
 	def was_published_recently(self):
 		now=timezone.now()
-		return now-dt.timedelta(days=1)<=self.pub_date<=now	
+		return now-dt.timedelta(days=1)<=self.pub_date<=now
+	was_published_recently.admin_order_field='pub_date'
+	was_published_recently.boolean=True
+	was_published_recently.short_description='Published recently?'
 	
 #@python_2_unicode_compatible
 class Choice(models.Model):
