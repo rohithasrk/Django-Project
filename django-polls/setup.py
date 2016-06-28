a2321b271317630e1c89b114ee550435a81f6585
Rohith Asrk <rohith.asrk@gmail.com>
2016-06-28 18:36:21 +0530

    Some changes in settings.py

--- django-polls/setup.py
+++ django-polls/setup.py
@@ -7,6 +7,7 @@ with open(os.path.join(os.path.dirname(__file__),'README.rst')) as readme:
 # allow setup.py to be run from any path
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+#print os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))
 
 setup(
 	name='django-polls',
@@ -34,4 +35,3 @@ setup(
 		'Topic :: Internet :: WWW/Http :: Dynamic Content',
 	],
 )
-
--- django-project/mysite/settings.py
+++ django-project/mysite/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#print BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': ['/home/rohith/Documents/projects/django-project/django-polls/polls/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +67,8 @@ TEMPLATES = [
         },
     },
 ]
+
+#print TEMPLATES[0]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
