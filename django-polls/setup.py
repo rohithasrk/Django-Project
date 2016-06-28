a565617505717022a48b350f2c8bc789c5720a26
Rohith Asrk <rohith.asrk@gmail.com>
2016-06-28 16:55:06 +0530

    Packagin

---
+++ django-polls/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__),'README.rst')) as readme:
+	README = readme.read()
+
+# allow setup.py to be run from any path
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+	name='django-polls',
+	version='0.1',
+	packages=find_packages(),
+	include_package_data=True,
+	license='MIT License',
+	description='A simple Django app to conduct Web-based polls.',
+	long_description=README,
+	url='www.rohithasrk.com',
+	author='Rohith A.S.R.K.',
+	author_email='rohith.asrk@gmail.com',
+	classifiers=[
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Framework :: Django :: 1.10',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Topic :: Internet :: WWW/Http',
+		'Topic :: Internet :: WWW/Http :: Dynamic Content',
+	],
+)
+
