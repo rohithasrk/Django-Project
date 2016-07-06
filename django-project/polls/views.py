7811892ed754b81e7aeae4b621d9401c0d1fa5ba
Rohith Asrk <rohith.asrk@gmail.com>
2016-07-07 02:13:59 +0530

    Added a login page

--- django-project/polls/views.py
+++ django-project/polls/views.py
@@ -5,8 +5,10 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
 from .forms import UserForm, UserProfileForm
+from django.template import loader
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
 
-#from django.template import loader
 
 class IndexView(generic.ListView):
 	template_name='polls/index.html'
@@ -65,3 +67,22 @@ def register(request):
 		profile_form = UserProfileForm()
 	
 	return render(request, 'polls/register.html',{'user_form':user_form, 'profile_form':profile_form,'registered':registered})
+
+def user_login(request):
+	if request.method='POST':
+		username=request.POST.get('username')
+		password=request.POST.get('password')
+		
+		user = authenticate(username=username,password=password)
+	
+		if user:
+			if user.is_active:
+				login(request,user)
+				return HttpResponseRedirect('/polls/')
+			else:
+				return HttpResponse("Disabled")
+		else:
+			print "Invalid login details: {0}{1}".format(username,password)
+			return HttpResponseRedirect("Invalid login details")
+	else:
+		return render(request,'polls/login.html',{})
