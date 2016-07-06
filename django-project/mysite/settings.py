399ddc9bad661c7cca511502749bd7f46391382d
Rohith Asrk <rohith.asrk@gmail.com>
2016-07-06 21:37:12 +0530

    Added password hashers

--- django-project/mysite/settings.py
+++ django-project/mysite/settings.py
@@ -102,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS= [
+	'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+	'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
